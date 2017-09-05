f43474cd2e3c5998a7d60475c0a8802417faa266
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2017-09-05 18:01:20 +0300

    Убрал доступ к тестовой странице

--- server.js
+++ server.js
@@ -3,45 +3,45 @@ const Config = require('./config.json').server;
 const path = require("path");
 const fs = require("fs");
 
-// let server = http.createServer();
+let server = http.createServer();
 
-let server = http.createServer( function(req, res) {
-    
-    let localPath;
-    if(req.method == 'GET') {
-        
-        localPath = path.join(__dirname, '/public/', "index.html");
-        
-        fs.exists(localPath, function(exists) {
-            if(exists) {
-                getFile(localPath, res, "text/html");
-            } else {
-                console.log("File not found: " + localPath);
-                res.writeHead(404);
-                res.end();
-            }
-        });
-    }
-    
-});
+// let server = http.createServer( function(req, res) {
+//
+//     let localPath;
+//     if(req.method == 'GET') {
+//
+//         localPath = path.join(__dirname, '/public/', "index.html");
+//
+//         fs.exists(localPath, function(exists) {
+//             if(exists) {
+//                 getFile(localPath, res, "text/html");
+//             } else {
+//                 console.log("File not found: " + localPath);
+//                 res.writeHead(404);
+//                 res.end();
+//             }
+//         });
+//     }
+//
+// });
 
 server.listen(Config.port, function() {
     require('./io').listen(server, function(){
-        console.log('server running at: ' + Config.host + ':' + Config.port);
+        console.log('Server running at: ' + Config.host + ':' + Config.port);
     });
 });
 
 
-function getFile(localPath, res, mimeType) {
-    fs.readFile(localPath, function(err, contents) {
-        if(!err) {
-            res.setHeader("Content-Length", contents.length);
-            res.setHeader("Content-Type", mimeType);
-            res.statusCode = 200;
-            res.end(contents);
-        } else {
-            res.writeHead(500);
-            res.end();
-        }
-    });
-}
+// function getFile(localPath, res, mimeType) {
+//     fs.readFile(localPath, function(err, contents) {
+//         if(!err) {
+//             res.setHeader("Content-Length", contents.length);
+//             res.setHeader("Content-Type", mimeType);
+//             res.statusCode = 200;
+//             res.end(contents);
+//         } else {
+//             res.writeHead(500);
+//             res.end();
+//         }
+//     });
+// }
